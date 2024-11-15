622ce55e0fcff88f0e53e2aa50021846f1af9463
Thanakit Chokbunsuwan <110766946+TeeThanakit@users.noreply.github.com>
2024-11-15 14:26:03 +0000

    fix calculation bug

--- calculator.py
+++ calculator.py
@@ -3,23 +3,26 @@ class Calculator:
         return a + b
 
     def subtract(self, a, b):
-        return b - a
+        return a - b
 
     def multiply(self, a, b):
         result = 0
-        for i in range(b+1):
+        if b < 0:
+            a = -a
+            b = -b
+        for i in range(b):
             result = self.add(result, a)
         return result
 
     def divide(self, a, b):
         result = 0
-        while a > b:
+        while a >= b:
             a = self.subtract(a, b)
             result += 1
         return result
     
     def modulo(self, a, b):
-        while a <= b:
+        while a >= b:
             a = a-b
         return a
 
